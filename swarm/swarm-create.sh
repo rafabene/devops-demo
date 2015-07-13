7e75d9763f77eed3c877a66c23c3fb845a4fbff7
Rafael Benevides <rafabene@gmail.com>
2015-07-13 15:32:03 -0300

    Add Swarm config

---
+++ swarm/swarm-create.sh
@@ -0,0 +1,9 @@
+echo "Creating cluster ..."
+TOKEN=`docker run swarm create`
+echo "Got the token " $TOKEN
+echo "Creating Swarm master ..."
+docker-machine create -d virtualbox --swarm --swarm-master --swarm-strategy "spread" --swarm-discovery  token://$TOKEN swarm-master
+echo "Creating Swarm node 01 ..."
+docker-machine create -d virtualbox --swarm --swarm-discovery token://$TOKEN swarm-node-01
+echo "Creating Swarm node 02 ..."
+docker-machine create -d virtualbox --swarm --swarm-discovery token://$TOKEN swarm-node-02
---
+++ swarm/swarm-destroy.sh
@@ -0,0 +1,2 @@
+echo "Destroying Swarm cluster ..."
+docker-machine rm swarm-master swarm-node-01 swarm-node-02
