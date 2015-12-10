5a8c83f4e79f6ebe3eecdeb4d75763b570bbbd9c
Rafael Benevides <rafabene@gmail.com>
2015-12-09 21:22:49 -0500

    Fix mod_cluster startup

--- Dockerfiles/mod_cluster/launch.sh
+++ Dockerfiles/mod_cluster/launch.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 # Get the IP address
-IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+#IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 
 # Adjust the IP addresses in the mod_cluster.conf file
-sed -i "s|[0-9\.\*]*:80|$IPADDR:80|g" /etc/httpd/conf.d/mod_cluster.conf
+#sed -i "s|[0-9\.\*]*:80|$IPADDR:80|g" /etc/httpd/conf.d/mod_cluster.conf
 
 
-echo "Running mod_cluster. Config file"
+echo "Running mod_cluster."
 
 # Run Apache
 httpd -D FOREGROUND
